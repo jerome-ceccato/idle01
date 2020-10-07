d7f3b2336f37bb85617deee81a91dcb83b73f37c
IATGOF <jerome.ceccato@gmail.com>
2020-10-07 21:58:11 +0200

    WIP yaml

---
+++ Assets/Core/Data/DataLoader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using YamlDotNet.Serialization;
+public class DataLoader
+{
+    private Deserializer deserializer;
+    public DataLoader()
+    {
+        deserializer = new Deserializer();
+        deserializer.Deserialize()
+    }
+
+}
