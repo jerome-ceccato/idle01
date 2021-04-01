92ac17e96f398a27fbc2bae7b19e2e394c830564
Jérôme Ceccato <jerome.ceccato@gmail.com>
2021-04-01 21:37:20 +0200

    Simple resource overview ui

--- Assets/Scripts/OverviewContainer.cs
+++ Assets/Scripts/OverviewContainer.cs
@@ -1,7 +1,27 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OverviewContainer : MonoBehaviour
 {
+    private Text contentText;
 
+    private void Start()
+    {
+        contentText = GetComponentInChildren<Text>();
+    }
+
+    void OnGUI()
+    {
+        Dictionary<ResourceEntity, BigInteger> ownedResources = GameManager.Instance.OwnedResources;
+
+        string content = string.Join("\n", ownedResources.Select((item) =>
+        {
+            return $"{item.Key.Name}: {item.Value}";
+        }));
+
+        contentText.text = content;
+    }
 }
